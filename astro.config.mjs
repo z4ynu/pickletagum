7c82df9699a4ec61b133e5a617a2d3d1f3e13abb
Zy <zennithnunez@gmail.com>
2026-07-29 09:09:42 +0800

    Fix Vercel analytics deployment runtime

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    }
-  })
+  output: 'static'
 });
